93fbdc50c32cdac2fc1c02de2e92285ed0af0c92
andyvanee <1andyvanee@gmail.com>
2019-01-02 11:11:24 -0700

    Basic routing test

---
+++ test/MiniRoute/MiniRouteTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MiniRoute;
+
+use PHPUnit\Framework\TestCase;
+
+class MyController {
+    public function __construct($container) {
+        // $this->view = $container->view;
+        // $this->store = $container->store;
+    }
+
+    public function index($request, $response) {
+        echo 'Hello World';
+    }
+}
+
+class MiniRouteTest extends TestCase {
+    protected function setUp() {
+        $_SERVER['REQUEST_URI'] = '/';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $this->app = new MiniRoute;
+        $this->app->route('GET', '/', [MyController::class, 'index']);
+    }
+
+    public function testEchoOutput() {
+        $output = "";
+        ob_start();
+        $this->app->run();
+        $output = ob_get_contents();
+        ob_end_clean();
+        $this->assertEquals('Hello World', $output);
+    }
+}
